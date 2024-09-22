11830b11a8bc758432dfc7d0d3bf8696f952c7a2
Gautampatel-28 <gautampatel7233@gmail.com>
2024-09-22 12:52:31 +0530

    bug fx

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <>
+        <section className='g-wrapper'>
+            <div className='paddings innerWidth g-container'>
+                <div className='flexColCenter inner-container'>
+                    <span className='primaryText'>Get Started with Homzy</span>
+                    <span className='secondaryText'>Subscribe and find super attractive price quotes from
+                    <br />
+                    Find your residence soon
+                    </span>
+                    <button className='button'>
+                        <a href="gautampatel7233@gmail.com">GetStarted</a>
+                    </button>
+                </div>
+            </div>
+        </section> 
+    </>
+  )
+};
+
+export default GetStarted;
--- src/utils/accordion.jsx
+++ src/utils/accordion.jsx
@@ -5,19 +5,19 @@ const data = [
     icon: <HiShieldCheck/>,
     heading: "Best interest rates on the market",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Dive into the sea of high-interest rates and watch your savings soar to new heights!",
   },
   {
     icon: <MdCancel />,
     heading: "Prevent unstable prices",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Keep your budget steady Prevent price volatility with smart strategies Unlock the door to your future, Find your ideal property with us.",
   },
   {
     icon: <MdAnalytics />,
     heading: "Best price on the market",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Maximize your investment: Grab the best price available in the market, Unlock unbeatable value Discover the best deals in real estate",
   },
 ];
 export default data;
